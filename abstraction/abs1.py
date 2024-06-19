286f5b8e91e95e7330ae14e7937fda753424d544
Vaishnavi Bhoyar <vaishubhoyar02@gmail.com>
2024-06-19 18:05:43 +0530

    secondcommit

---
+++ abstraction/abs1.py
@@ -0,0 +1,9 @@
+
+# abstraction = Abstraction is used to hide the internal functionality 
+# of the function from the users. The users only interact with the basic 
+# implementation of the function, but inner working is hidden. 
+# User is familiar with that "what function does" but they don't know 
+# "how it does.
+
+def add(x,y):
+    return x+y
---
+++ abstraction/abs3.py
@@ -0,0 +1,8 @@
+
+
+import abs1
+import abs2
+add=abs1.add(4,5)
+sub=abs2.sub(20,10)
+print("Addition=",add)
+print("Substraction=",sub)
