28c847460d8aa7a0493468746a1da36df1d0c12d
Vaishnavi Bhoyar <vaishubhoyar02@gmail.com>
2024-06-20 13:13:12 +0530

    change

--- encapsulation/encap.py
+++ encapsulation/encap.py
@@ -5,29 +5,42 @@
 # Pyhton does not recommend protected modifier
 
 # Private
-class Student:
-    school="MVM"
-    def __init__(self,name,age):
-        self.__name=name
-        self.__age=age
-    def details(self):
-        print("Name=",self.__name)
-        print("Age=",self.__age)
-        print("School=",Student.school)
-class Marks(Student):
-    def marks(self,hindi,english):
-        self.hindi=hindi
-        self.english=english
-    def complete_details(self):
-        print("Name=",self.__name)
-        print("Age=",self.__age)
-        print("School=",Student.school)
-        print("Hindi=",self.hindi)
-        print("English=",self.english)
-obj=Marks("Vaishu",22)
-obj.__name="Maanu bhoyar"
-obj.details()
-obj.marks(78,87)
-Student.school="Maharishi Vidhya Mnadir"
-obj.complete_details
+# class Student:
+#     school="MVM"
+#     def __init__(self,name,age):
+#         self.__name=name
+#         self.__age=age
+#     def details(self):
+#         print("Name=",self.__name)
+#         print("Age=",self.__age)
+#         print("School=",Student.school)
+# class Marks(Student):
+#     def marks(self,hindi,english):
+#         self.hindi=hindi
+#         self.english=english
+#     def complete_details(self):
+#         print("Name=",self.__name)
+#         print("Age=",self.__age)
+#         print("School=",Student.school)
+#         print("Hindi=",self.hindi)
+#         print("English=",self.english)
+# obj=Marks("Vaishu",22)
+# obj.__name="Maanu bhoyar"
+# obj.details()
+# obj.marks(78,87)
+# Student.school="Maharishi Vidhya Mnadir"
+# obj.complete_details
 
+
+# Constructor
+class Faculty:
+    def __init__(self,a,b,c):
+        self.id=a
+        self.name=b
+        self.age=c
+    def display(self):
+        print("Faculty id :",self.id)
+        print("Faculty name :",self.name)
+        print("Faculty age :",self.age)
+a=Faculty(1,"Vaishu",22)
+a.display()        
