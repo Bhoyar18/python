3e70ebe7095d763878dbaaa5c2c1ff0a9b8b6100
Vaishnavi Bhoyar <vaishubhoyar02@gmail.com>
2024-06-20 13:54:48 +0530

    change

--- inheritance/types.py
+++ inheritance/types.py
@@ -1,16 +1,20 @@
 
 # Types of Inheritance
-# SINGLE INHERITANCE = Single Inheritance is the simplest form of 
+# 1.SINGLE INHERITANCE = Single Inheritance is the simplest form of 
 # inheritance where a single child class is derived from a single parent class
 
-class Parent:                  
-    # parent class
-    def hello1(self):
-        print("This is Parrent Class")
-class Child(Parent):    
-  # child class
-    def hello2(self):               
-        print("This is Child Class")                            
-a = Child()                 
-a.hello1()                 
-a.hello2()         
+# class Parent:                  
+#     # parent class
+#     def hello1(self):
+#         print("This is Parrent Class")
+# class Child(Parent):    
+#   # child class
+#     def hello2(self):               
+#         print("This is Child Class")                            
+# a = Child()                 
+# a.hello1()                 
+# a.hello2()         
+
+# 2.MULTIPLE INHERITANCE = In multiple inheritance, a single child 
+# class is inherited from two or more parent(base) classes. It means the 
+# child(derived) class has access to all the parent classes' methods and attributes.
