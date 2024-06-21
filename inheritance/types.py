7f7a6be238a968a4250172ba70c1a7f911ef5a4b
Vaishnavi Bhoyar <vaishubhoyar02@gmail.com>
2024-06-21 12:16:47 +0530

    fifth

--- inheritance/types.py
+++ inheritance/types.py
@@ -18,24 +18,29 @@
 # 2.MULTIPLE INHERITANCE = In multiple inheritance, a single child 
 # class is inherited from two or more parent(base) classes. It means the 
 # child(derived) class has access to all the parent classes' methods and attributes.
-class Grandparent: 
-    house = 'Three storeyed building' 
-    def age(self, number): 
-        if number>70: 
-            return ('Much Older!') 
-        else: 
-            return ('Growing towards Old age.')        
-class Parent: 
-    car = 'Swift' 
-    def work(self, hours): 
-        if hours>40: 
-            return ('Very hard working!') 
-        else: 
-            return ('Average hard working!') 
-class Child(Parent, Grandparent): 
-    phone = 'Samsung'  
-object = Child() 
-print('My house is a ',object.house, ' and I have a ', object.car, 
-      ' and a ', object.phone,' phone.') 
-print('My grandparent is 78 years old. He is ', object.age(78)) 
-print('My father works for 47 hours a week. He is ', object.work(47))
+# class Grandparent: 
+#     house = 'Three storeyed building' 
+#     def age(self, number): 
+#         if number>70: 
+#             return ('Much Older!') 
+#         else: 
+#             return ('Growing towards Old age.')        
+# class Parent: 
+#     car = 'Swift' 
+#     def work(self, hours): 
+#         if hours>40: 
+#             return ('Very hard working!') 
+#         else: 
+#             return ('Average hard working!') 
+# class Child(Parent, Grandparent): 
+#     phone = 'Samsung'  
+# object = Child() 
+# print('My house is a ',object.house, ' and I have a ', object.car, 
+#       ' and a ', object.phone,' phone.') 
+# print('My grandparent is 78 years old. He is ', object.age(78)) 
+# print('My father works for 47 hours a week. He is ', object.work(47))
+
+# 3.MULTILEVEL INHERITANCE = A type of inheritance where a class is
+#  derived from a class which is also derived from another class
+# for example:Class C inherits from Class B, and Class B inherits 
+# from Class A.
