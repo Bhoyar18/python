7b243ba3e0ac9e8ee8f99d07cdbaba4dfc499d77
Vaishnavi Bhoyar <vaishubhoyar02@gmail.com>
2024-06-22 16:32:08 +0530

    final

--- polymorphism/poly.py
+++ polymorphism/poly.py
@@ -36,3 +36,28 @@ class B(A):
         super().add(30,40)
 obj=B()
 obj.add(10,20)
+
+
+# implementing polymorphism using function
+class India():
+    def capital(self):
+        print("New Delhi is the capital of India.")
+    def language(self):
+        print("Hindi is the most widely spoken language of India.")
+    def type(self):
+        print("India is a developing country.")
+class USA():
+    def capital(self):
+        print("Washington, D.C. is the capital of USA.")
+    def language(self):
+        print("English is the primary language of USA.")
+    def type(self):
+        print("USA is a developed country.")
+def func(obj):
+    obj.capital()
+    obj.language()
+    obj.type()
+obj_ind = India()
+obj_usa = USA()
+func(obj_ind)
+func(obj_usa)
