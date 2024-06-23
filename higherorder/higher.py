de03df2856cb3b74860133ee450cfa4e289d8c20
Vaishnavi Bhoyar <vaishubhoyar02@gmail.com>
2024-06-23 16:29:51 +0530

    change3

--- higherorder/higher.py
+++ higherorder/higher.py
@@ -26,8 +26,29 @@ from functools import reduce
 # x=reduce(sum,my_list)
 # print(x)    
 
-n=int(input("Enter any number"))
-def fact(x):
-     return x*x-1
-x=reduce()
-print(x)
+# n=int(input("Enter any number"))
+# def fact(x):
+#      return x*x-1
+# x=reduce()
+# print(x)
+
+# map function()= jab saare objects ke upar same operations 
+# perform krna hota hai to map function ka use hota hai
+
+# list1=[5,10,15,20,25]
+# def add(n):
+#     return n+5
+# x=map(add,list1)
+# print(x)
+# print(tuple(x))
+# print(list(x))
+
+
+# my_list=[10,25,30,45,50,65,70,85,23]
+# def odd_even(n):
+#     if n%2==0:
+#         return 'even'
+#     else:
+#         return 'odd'
+# x=list(map(odd_even,my_list))
+# print(x)
